78dd14ead3a4587c5d589cbc7aafbc2a4a6c308d
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-03-01 16:30:09 -0800

    next js updated for meta data

---
+++ app/(Pages)/(News)/news/page.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "News | South Bay Math Circle",
+    icons: {
+        icon: '/sun.png',
+    }
+  };
+export default function News() {
+  return (
+    <></>
+  )
+}
